fb8e8fad96b04ba524de3dc65f9db093ef6e1498
Bernard Rodier <bernard.rodier@gmail.com>
2022-07-31 09:56:05 +0200

    package ready to solve codinggame challange

--- rubik/transform.go
+++ rubik/transform.go
@@ -1,9 +1,14 @@
 package rubik
 
+const Corner = 0 // Sommet Corner
+const Border = 1 // Border
+const Center = 2 // Center
+
 type MoveElem struct {
-	from int
-	to   int
-	flip int
+	piece int
+	from  int
+	to    int
+	flip  int
 }
 
 type Transform []MoveElem
@@ -12,14 +17,14 @@ var U Transform
 
 func init() {
 	U = Transform{
-		0: MoveElem{from: 0, to: 1, flip: 8},
-		1: MoveElem{from: 1, to: 3, flip: 16},
-		2: MoveElem{from: 3, to: 2, flip: 16},
-		3: MoveElem{from: 2, to: 0, flip: 8},
-		4: MoveElem{from: 8, to: 9, flip: 12},
-		5: MoveElem{from: 9, to: 10, flip: 12},
-		6: MoveElem{from: 10, to: 11, flip: 12},
-		7: MoveElem{from: 11, to: 8, flip: 12},
+		0: MoveElem{piece: Corner, from: 0, to: 1, flip: 8},
+		1: MoveElem{piece: Corner, from: 1, to: 3, flip: 16},
+		2: MoveElem{piece: Corner, from: 3, to: 2, flip: 16},
+		3: MoveElem{piece: Corner, from: 2, to: 0, flip: 8},
+		4: MoveElem{piece: Border, from: 0, to: 1, flip: 12},
+		5: MoveElem{piece: Border, from: 1, to: 2, flip: 12},
+		6: MoveElem{piece: Border, from: 2, to: 3, flip: 12},
+		7: MoveElem{piece: Border, from: 3, to: 0, flip: 12},
 	}
 }
 
@@ -27,7 +32,14 @@ func (t Transform) Apply(r Rubik) (tr Rubik) {
 	tr = make(Rubik, 26)
 	copy(tr, r)
 	for _, me := range t {
-		tr[me.to] = flip(r[me.from], me.flip)
+		offset := 0
+		switch me.piece {
+		case Border:
+			offset += bordersIdxOffset
+		case Center:
+			offset += centersIdxOffset
+		}
+		tr[me.to+offset] = flip(r[me.from+offset], me.flip)
 	}
 	return
 }
@@ -45,20 +57,22 @@ func NewTransform(from Rubik, to Rubik) Transform {
 
 func InitialTransform(to Rubik) Transform {
 	st := make(Transform, 0)
-	for i, v := range to[:nbCorner] {
-		me := MoveElem{i, v % nbCorner, v - (v % nbCorner)}
+	for i, v := range to[:bordersIdxOffset] {
+		cid := v % nbCorner
+		me := MoveElem{Corner, cid, i, v - cid}
 		if me.from != me.to || me.flip != 0 {
 			st = append(st, me)
 		}
 	}
 	for i, v := range to[bordersIdxOffset:centersIdxOffset] {
-		me := MoveElem{i - bordersIdxOffset, v % nbBorder, v - (v % nbBorder)}
+		bId := v % nbBorder
+		me := MoveElem{Border, bId, i, v - bId}
 		if me.from != me.to || me.flip != 0 {
 			st = append(st, me)
 		}
 	}
 	for i, v := range to[centersIdxOffset:] {
-		me := MoveElem{i - centersIdxOffset, v, 0}
+		me := MoveElem{Center, v, i, 0}
 		if me.from != me.to || me.flip != 0 {
 			st = append(st, me)
 		}
@@ -69,7 +83,7 @@ func InitialTransform(to Rubik) Transform {
 func (t Transform) Reverse() Transform {
 	rt := make(Transform, len(t))
 	for i, me := range t {
-		rt[i] = MoveElem{me.to, me.from, reverseFlip(me.flip)}
+		rt[i] = MoveElem{me.piece, me.to, me.from, reverseFlip(me.flip)}
 	}
 	return rt
 }
--- rubikapp.go
+++ rubikapp.go
@@ -16,17 +16,21 @@ func main() {
 	fmt.Printf("%v\n", r)
 	r.Display(os.Stdout)
 	fmt.Println("===== Now move up =======")
+	fmt.Printf("U Tranform object : %v\n", rubik.U)
 	r.MoveUpDirect()
 	fmt.Printf("%v\n", r)
 	r.Display(os.Stdout)
 
-	t := rubik.InitialTransform(*r).Reverse()
+	t := rubik.InitialTransform(*r)
+	fmt.Printf("Tranform object : %v\n", t)
+	t = t.Reverse()
+	fmt.Printf("Reverse tranform object : %v\n", t)
 	newRubik := t.Apply(*r)
 	fmt.Printf("%v\n", newRubik)
 	newRubik.Display(os.Stdout)
 	fmt.Println("===== Re-apply transform =======")
 	newRubik = t.Apply(newRubik)
 	fmt.Printf("%v\n", newRubik)
-	r.Display(os.Stdout)
+	newRubik.Display(os.Stdout)
 
 }
